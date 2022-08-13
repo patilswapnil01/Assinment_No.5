8b593cbfc6b751e8e51b06eeb09ac6ff991cecfe
patilswapnil01 <102236192+patilswapnil01@users.noreply.github.com>
2022-08-13 19:17:21 +0530

    Add files via upload

---
+++ que_1.py
@@ -0,0 +1,22 @@
+# Write a Python class to implement pow(x, n)
+class py_solution:
+   def pow(self, x, n):
+        if x==0 or x==1 or n==1:
+            return x 
+
+        if x==-1:
+            if n%2 ==0:
+                return 1
+            else:
+                return -1
+        if n==0:
+            return 1
+        if n<0:
+            return 1/self.pow(x,-n)
+        val = self.pow(x,n//2)
+        if n%2 ==0:
+            return val*val
+        return val*val*x
+x = int(input("Enter the base value: "))
+n = int(input("Enter the power value: "))
+print(py_solution().pow(x,n))
